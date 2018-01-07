65b260edf04d6437073211271309fbeae245edb4
igor.bogoslavskyi@uni-bonn.de <igor.bogoslavskyi@uni-bonn.de>
2018-01-07 18:52:45 +0200

    Add dummy test and update gitignore

---
+++ tests/test_checker.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Test the checker."""
+
+import unittest
+
+# from .src.checker import Checker
+
+
+class TestChecker(unittest.TestCase):
+    """Test the checker."""
+
+    def testSuccess(self):
+        """Dummy test."""
+        self.assertEqual(1, 1)

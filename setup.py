bd489686a88ecf785b5967ac1337b8068acc44db
Igor Bogoslavskyi <igor.bogoslavskyi@gmail.com>
2018-12-17 18:58:48 +0100

    Update version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils import log
 from setuptools import setup
 from setuptools.command.install import install
 
-VERSION_STRING = '0.0.1'
+VERSION_STRING = '0.0.2'
 
 PACKAGE_NAME = 'ipb_homework_checker'
 
